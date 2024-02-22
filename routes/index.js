ffc309ccf346ea5f5dd2a0d1dab90b9341d1d78c
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2024-02-21 22:01:47 -0800

    development done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+const messages = [
+  {
+    text: "the job market is horrible right now!",
+    user: "me",
+    added: new Date(),
+  },
+  {
+    text: "Hope i will land a job soon",
+    user: "me",
+    added: new Date(),
+  },
+
+]
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Drop your thoughts about the current web dev job market', messages: messages });
 });
+
+router.get('/new', function(req, res){
+  res.render('form')
+})
+
+router.post('/new', function(req, res){
+  messages.push({text:req.body.newMessage, user: req.body.newAuthor, added: new Date()})
+  res.redirect('/')
+})
+
 
 module.exports = router;
